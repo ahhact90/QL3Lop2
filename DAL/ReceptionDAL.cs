6ad6959c596f5c65fd6eede6ec230a8f1fc64171
ahhact90 <phugia1990@gmail.com>
2016-12-02 09:07:19 +0700

    DAL ReceptionDAL update

---
+++ DAL/ReceptionDAL.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DAL
+{
+    public class ReceptionDAL : BaseDAL, UTL.IBaseDAL
+    {
+        #region Implement
+
+        #endregion
+
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object GetByKey(object key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable Search(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable Select()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable Select(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet Select_non()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet Select_non2(string n)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataSet Select_non3(DateTime to, DateTime from)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
